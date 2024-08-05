ccffd4b05f713c19225f14555708f726a7e730f4
tanuja009 <tanujapatidar01@gmail.com>
2024-08-06 01:46:53 +0530

    changes

--- project2/myproject/myapp/views.py
+++ project2/myproject/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return render(request,"homepage.html")
 def add(request):
   return render(request,"addrecipe.html")
--- project2/myproject/myproject/urls.py
+++ project2/myproject/myproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.add,name="add")
+    path('add/',views.add,name="add"),
+    path('',views.home,name="home")
 ]
